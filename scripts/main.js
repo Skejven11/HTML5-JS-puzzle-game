9381b002c04bf140dd7cfcb29e389e9685ee3710
Skejven11 <Zysku23456@gmail.com>
2021-04-21 16:14:38 +0200

    Starting menu and music

--- scripts/main.js
+++ scripts/main.js
@@ -5,17 +5,17 @@ import { maps } from './maps.js';
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 ctx.globalCompositeOperation='destination-over';
-
 canvas.width = 400;
 canvas.height = 400;
-
 const level = new maps();
 level.loadlevel(0);
-
 const player = new Player(level);
-new InputHandler(player);
-
 level.drawMap(ctx);
+const startButton = document.getElementById("start-button");
+
+startButton.addEventListener("click", function(){
+    startGame();
+});
 
 function gameLoop() {
     ctx.clearRect(0,0,400,400);
@@ -24,5 +24,11 @@ function gameLoop() {
     requestAnimationFrame(gameLoop);
 }
 
-gameLoop();
-
+function startGame() {
+    document.getElementsByClassName("canvas-menu")[0].style.visibility = "hidden";
+    var music = document.getElementById("background-music");
+    music.volume = 0.4;
+    music.play();
+    new InputHandler(player);
+    gameLoop();
+}
